36961328ec75961fb3b4d3fe89e6f176ff7a6148
Andres Higueros <higueros71@hotmail.com>
2019-10-08 14:38:25 -0600

    agregando simulador de collapse en el área de administración

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  showComponentA: boolean;
+  showComponentB: boolean;
+  showComponentC: boolean;
+  showComponentD: boolean;
+
+  constructor() {
+    this.showComponentA = true;
+    this.showComponentB = true;
+    this.showComponentC = true;
+    this.showComponentD = true;
+  }
 
   ngOnInit() {
   }
 
+  show(component: boolean) {
+    if (!component) {
+       return component = true;
+    } else {
+      return component = false;
+    }
+  }
+
+  showA() {
+    if (!this.showComponentA) {
+       return this.showComponentA = true;
+    } else {
+      return this.showComponentA = false;
+    }
+  }
+  showB() {
+    if (!this.showComponentB) {
+       return this.showComponentB = true;
+    } else {
+      return this.showComponentB = false;
+    }
+  }
+  showC() {
+    if (!this.showComponentC) {
+       return this.showComponentC = true;
+    } else {
+      return this.showComponentC = false;
+    }
+  }
+
+  showD() {
+    if (!this.showComponentD) {
+       return this.showComponentD = true;
+    } else {
+      return this.showComponentD = false;
+    }
+  }
 }
