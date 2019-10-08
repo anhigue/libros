6791b32797adb7e8ea9960cf294d09132c3b0e24
Andres Higueros <higueros71@hotmail.com>
2019-10-07 21:21:47 -0600

    Agregando base de proyecto, angular 7

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {
+  AccordionModule,
+  AlertModule,        // Foundation Callouts
+  ButtonsModule,
+  CarouselModule,     // Foundation Orbit
+  CollapseModule,
+  BsDatepickerModule,
+  BsDropdownModule,   // Foundation Dropdown Menus and Dropdown Panes
+  ModalModule,        // Foundation Reveal
+  OffcanvasModule,
+  PaginationModule,
+  ProgressbarModule,
+  RatingModule,
+  SortableModule,
+  TabsModule,
+  TimepickerModule,
+  TooltipModule,
+  TypeaheadModule,
+} from 'ngx-foundation';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AccordionModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
