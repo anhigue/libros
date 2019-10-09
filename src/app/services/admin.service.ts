16591f8f8464c024320bee1d10e20d7f62974236
Andres Higueros <higueros71@hotmail.com>
2019-10-08 23:26:07 -0600

    agregando los servicios para poder crear un nuevo articulo

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -14,19 +14,53 @@ export class AdminService {
     this.base = 'http://localhost/articulos/routes/';
   }
 
+  // obtiene los roles del sistema
   getRol() {
     return this.http.get(this.base + 'administracion/read.tipo.usuario.php', { headers: this.headers });
   }
 
+  // obtiene los generos para registro de un nuevo usuario
   getGenero() {
     return this.http.get(this.base + 'administracion/read.genero.php', { headers: this.headers });
   }
 
+  // obtiene la lista de las categorias y subcategorias
   getCategoriaSub() {
     return this.http.get(this.base + 'administracion/read.all.categoria.php', { headers: this.headers });
   }
 
+  // obtiene la lista de los usuarios
   getUsuarios() {
     return this.http.get(this.base + 'administracion/read.datos.usuario.php', { headers: this.headers });
+  }
+
+  // obtiene la lista de los estados que puede tener un articulo
+  getEstados() {
+    return this.http.get(this.base + 'administracion/read.estados.php', { headers: this.headers });
+  }
+
+  // crear un encabezado del articulo {"titulo": "Este es mi titulo","fk_id_estado": 1,"plantilla_articulo": 1}
+  createArticulo(articulo: any) {
+    return this.http.post(this.base + 'articulos/create.php', articulo, { headers: this.headers });
+  }
+
+  // crear los parrafos de un articulo { "parrafo":" este es el parrafo", "id_articulo": 1}
+  crateParrafo(parrafo: any) {
+    return this.http.post(this.base + 'articulos/create.parrafo.php', parrafo, { headers: this.headers });
+  }
+
+  // crear una imagen para el articulo { "id_articulo", "path"}
+  createImagen(imagen: any) {
+    return this.http.post(this.base + 'articulos/create.image.php', imagen, { headers: this.headers });
+  }
+
+  // crear un link para un articulo { "id_articulo": 3, "link": "www.google.com"}
+  createLink(link: any) {
+    return this.http.post(this.base + 'articulos/create.link.php', link, { headers: this.headers });
+  }
+
+  // eliminar un articulo { "id_articulo": 3}
+  deleteArticulo(articulo: any) {
+    return this.http.post(this.base + 'articulos/delete.php', articulo, { headers: this.headers });
   }
 }
