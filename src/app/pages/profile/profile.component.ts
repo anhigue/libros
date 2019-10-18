ad696dbbf79572e818a2ad0fae5f69ca7cce1a0a
Andres Higueros <higueros71@hotmail.com>
2019-10-18 13:38:23 -0600

    agregando logica para poder visualizar que tipo de informacion mostrar

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -10,6 +10,12 @@ export class ProfileComponent implements OnInit {
   usuario: any;
   oneAtATime = true;
   usuarioInfo: any;
+  tipoUsuario: any;
+
+  // show permisos
+  showModetion = false;
+  showAdministracion = false;
+  showSubscripcion = false;
 
   constructor(private admin: AdminService) {
     this.getUsuario();
@@ -28,9 +34,44 @@ export class ProfileComponent implements OnInit {
     const usuario = {
       id: this.usuario.id_usuario
     };
-    this.admin.getViewUsuario(usuario).toPromise().then(res => {
+    await this.admin.getViewUsuario(usuario).toPromise().then( async (res: any) => {
       this.usuarioInfo = res;
+      await this.admin.getRol().toPromise().then( async (rest: object[]) => {
+        this.tipoUsuario = await rest.filter( (value: any) => this.usuarioInfo.id_tipo === value.id_tipo_usuario)[0];
+        console.log(this.tipoUsuario);
+        this.showAdmin(this.tipoUsuario.tipo_usuario);
+        this.showModeracion(this.tipoUsuario.tipo_usuario);
+        this.showSub(this.tipoUsuario.tipo_usuario);
+      });
       console.log(this.usuarioInfo);
     });
+  }
+
+  showModeracion(tipoUsuario) {
+    if ( tipoUsuario !== 'moderador') {
+      this.showModetion = false;
+    } else {
+      this.showModetion = true;
+    }
+
+    console.log(this.showModetion, 'moderation');
+  }
+
+  showAdmin(tipoUsuario) {
+    if ( tipoUsuario !== 'administrador') {
+      this.showAdministracion = false;
+    } else {
+      this.showAdministracion = true;
+    }
+    console.log(this.showModetion, 'administrador');
+  }
+
+  showSub(tipoUsuario) {
+    if ( tipoUsuario !== 'autor') {
+      this.showAdministracion = false;
+    } else {
+      this.showAdministracion = true;
+    }
+    console.log(this.showModetion, 'autor');
   }
 }
